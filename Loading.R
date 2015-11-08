5dc539740d8ce8c00a86a03f2a952c4ccffc5042
Hans Chung <sunboy1120@gmail.com>
2015-11-07 23:00:20 -0600

    Create Loading.R

---
+++ Loading.R
@@ -0,0 +1,18 @@
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+
+mydata <- read.table(file, header=T, sep=";")
+mydata$Date <- as.Date(mydata$Date, format="%d/%m/%Y")
+df <- mydata[(mydata$Date=="2007-02-01") | (mydata$Date=="2007-02-02"),]
+
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
